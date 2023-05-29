e76470536fe7bd240306c8496006d4ac298b9471
sthbibek <bibek.alpode@gmail.com>
2023-05-29 22:05:24 +0545

    initial commit

---
+++ grunt-settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+  sassDir: 'scss/',
+  sassMainFileName: 'style',
+  cssDir: 'assets/',
+  cssMainFileDir: 'assets/',
+  cssMainFileName: 'style',
+  jsDir: 'js/',
+  imgDir: 'assets/images/',
+  imgSourceDir: 'sfdgassets/sourceimages/',
+
+  // sftp server
+  sftpServer: 'example.com',
+  sftpPort: '2121',
+  sftpLogin: 'login',
+  sftpPas: 'password',
+  sftpDestination: '/pathTo/css'
+};
